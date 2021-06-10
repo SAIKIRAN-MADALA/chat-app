1aba236c16331c1e2537a174f0c88fb480daac01
Saikiran <saikiranmadala10@gmail.com>
2021-06-11 01:22:43 +0530

    added html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express=require('express')
+const path=require('path')
 
 const app=express()
-const port=3000
+const port=process.env.PORT||3000
+const publicDirectoryPath= path.join(__dirname,'../public')
 
 
+app.use(express.static(publicDirectoryPath))
 
+
+app.listen(port,()=>{
+    console.log('='.repeat(50))
+       console.log(`server has started on port ${port}!`)
+    console.log('='.repeat(50))
+})
